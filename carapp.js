269f44afe6f34805b145c33123cc894cdca26bae
Chris Gramme <cgramme36@gmail.com>
2015-06-12 01:53:13 -0400

    jhg

--- carapp.js
+++ carapp.js
@@ -1,20 +1,10 @@
 var $halfWidth;
 var $headingWidth;
 var $iframeWidth;
-
-
 $(window).ready(function(){
     $('.created-by-text').fadeIn(1000);
 });
-
-
 $(document).ready(function(){
-    $('body').click(function(){
-        alert($(window).width());
-    });
-
-
-	
       //slideshow
 	  var _SlideshowTransitions = [
             //Custom slide show transitions. You can add as many as you like. Will start from top first.
@@ -24,15 +14,15 @@ $(document).ready(function(){
         var options = {
             $AutoPlay: true,
             $SlideshowOptions: {
-                    $Class: $JssorSlideshowRunner$,
-                    $Transitions: _SlideshowTransitions,
-                    $TransitionsOrder: 1,
-                    $ShowLink: true
-                }
+                $Class: $JssorSlideshowRunner$,
+                $Transitions: _SlideshowTransitions,
+                $TransitionsOrder: 1,
+                $ShowLink: true
+            }
         };
         //start slideshow
         var jssor_slider1 = new $JssorSlider$('slider1_container', options);
-
+        //Scales the slideshow container and adjusts picture size relative to screen size.
         function ScaleSlider() {
             var parentWidth = $('#slider1_container').width();
             if (parentWidth) {
@@ -41,22 +31,16 @@ $(document).ready(function(){
             else
                 window.setTimeout(ScaleSlider, 30);
         }
-
-        ScaleSlider();
-
-        $(window).bind("load", ScaleSlider);
-        $(window).bind("resize", ScaleSlider);
-        $(window).bind("orientationchange", ScaleSlider);
-
+    ScaleSlider();
+    $(window).bind("load", ScaleSlider);
+    $(window).bind("resize", ScaleSlider);
+    $(window).bind("orientationchange", ScaleSlider);
     resizeWindow();
 });
-
-
 //on window resize center and optimize
 $(window).resize(function(){
     resizeWindow();
 });
-
 function resizeWindow(){
     $iframeWidth = $('iframe').width();
     $('iframe').css('height', $iframeWidth);
